6b253a4ba5ee2a8cc8454d90e280df16f5ac35b0
Narasimha Swamy <swamy.hajeed@gmail.com>
2020-06-14 12:02:49 +0530

    For loop

--- main.go
+++ main.go
@@ -4,16 +4,13 @@ import "fmt"
 
 func main() {
 
-	var a int = 1
-	fmt.Printf("Declaration and Initialization of a %d\n", a)
-
-	var a1 = 1
-	fmt.Printf("Without explicit type declaration %d\n", a1)
-
-	a2 := 1
-	fmt.Printf("Short hand notation of declaration and initialization %d\n", a2)
-
-	var z int
-	fmt.Printf("Declaration and zero value initialization of z %d", z)
+	i := 0
+	for i < 3 {
+		i = i + 1
+		fmt.Printf("Python style of looping %d\n", i)
+	}
+	for j := 0; j < 3; j++ {
+		fmt.Printf("Initialization and iteration %d\n", j)
+	}
 
 }
