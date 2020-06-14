7ba2349e625cc0acc43665f87aab02f1bf56aa45
Narasimha Swamy <swamy.hajeed@gmail.com>
2020-06-14 11:49:54 +0530

    Variables and variours of initialization and declaration

--- main.go
+++ main.go
@@ -3,7 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Printf("Welcome to GO Strings!!\n")
 
-	fmt.Printf("String concatination exanple : %s ", "Ardhani "+"Narasimha"+" Swamy")
+	var a int = 1
+	fmt.Printf("Declaration and Initialization of a %d\n", a)
+
+	var a1 = 1
+	fmt.Printf("Without explicit type declaration %d\n", a1)
+
+	a2 := 1
+	fmt.Printf("Short hand notation of declaration and initialization %d\n", a2)
+
+	var z int
+	fmt.Printf("Declaration and zero value initialization of z %d", z)
+
 }
